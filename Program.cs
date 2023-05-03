13b63c08a3f425be5b22dcd28d3dc3123745b706
FelipeAp1903 <131301360+FelipeAp1903@users.noreply.github.com>
2023-05-03 17:02:42 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1;
+            int a2;
+
+            Console.Write("insira o Primeiro algarismo: ");
+            a1 = int.Parse(Console.ReadLine());
+
+            Console.Write("insira o Segundo algarismo Diferente ao segundo: ");
+            a2 = int.Parse(Console.ReadLine());
+
+            if (a1 > a2)
+                Console.WriteLine("O Primeiro ({0}) é maior que o Segundo ({1})", a1, a2);
+            else
+                Console.WriteLine("O Segundo ({1}) é maior que o Primeiro ({0})", a1, a2);
+        }
+    }
+}
